898c8ef95f64e0485b4c211dac3a8487a04f87ca
mike_varona <migueluk91@gmail.com>
2023-01-22 12:16:30 +0000

    Adding a "Header" Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from "./components/Layout/Header";
+import './App.css';
 
 const App = () => {
     return (
-        <div>
-        <h1>React Boilerplate</h1>
-        </div>
+        <Fragment>
+            <Header />
+        </Fragment>
     );
 }
 
